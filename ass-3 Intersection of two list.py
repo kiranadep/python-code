f298907ddf5ebd50c85f721b88d2ceafdbad0281
kiranadep <93147241+kiranadep@users.noreply.github.com>
2022-07-04 23:15:05 +0530

    Add files via upload

---
+++ ass-3 Intersection of two list.py
@@ -0,0 +1,13 @@
+l1 =[]
+n1 = int(input("Enter how many values do you want :"))
+for i in range(n1):
+    l1n1 = int(input(f"Enter value {i+1} :"))
+    l1.append(l1n1)
+
+l2=[]
+n2 = int(input("Enter how many values do you want :"))
+for i in range(n2):
+    l2n2 = int(input(f"Enter value {i+1} :"))
+    l2.append(l2n2)
+l3 = list(set(l1).intersection(l2))
+print("the l1 and l2 of union is  :",l3)
---
+++ reverse the list.py
@@ -0,0 +1,7 @@
+l = []
+n = int(input('Enter number of elements='))
+for i in range(0, n):
+    ele = int(input("enter element= "))
+    l.append(ele)
+l.reverse()
+print("Reverse order is:", l)
