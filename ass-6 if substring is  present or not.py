cb688d38350b1f9a6e02625421db67589d5d9938
kiranadep <93147241+kiranadep@users.noreply.github.com>
2022-07-05 08:47:11 +0530

    Update ass-6 if substring is  present or not.py

--- ass-6 if substring is  present or not.py
+++ ass-6 if substring is  present or not.py
@@ -4,3 +4,12 @@ if sub_str in main_string:
    print("Sub-string is present in string")
 else:
     print("Sub-String is not present in string")
+      
+# using find()
+
+main_string = input("Enter the String = ")
+sub_str = input("Enter the Sub-String = ")
+if ((main_string.find(sub_str)) == -1):
+   print("Sub-string is not present in string")
+else:
+    print("Sub-String is present in string")
